4ca321580d28b9e4c50b721e9af0d84f109722fc
Moura <hiroganja@gmail.com>
2022-04-24 16:46:56 -0300

    att contato html :)

--- Códigos/assets/js/main.js
+++ Códigos/assets/js/main.js
@@ -31,13 +31,25 @@ modalClose.forEach((mc) =>{
 
 
 /* filtro portfolio */
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 
 /* Link active work */ 
+const linkWork = document.querySelectorAll('.work__item')
 
+function activeWork(){
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
 
-/*=============== SWIPER TESTIMONIAL ===============*/
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
